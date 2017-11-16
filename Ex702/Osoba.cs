051a46e74fc23a0f9be2db691f8a7adee60d9a9a
Piotrek <@>
2017-11-16 12:21:53 +0100

    przeslanie plików

---
+++ Ex702/Osoba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex702
+{
+    class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+        private int rok;
+
+        public Osoba(string imie, string nazwisko, int rok)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.rok = rok;
+        }
+
+        public override string ToString()
+        {
+            return imie+" "+nazwisko+" "+rok;
+        }
+    }
+}
---
+++ Ex702/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex702
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Osoba> osoby = new List<Osoba>();
+            osoby.Add(new Osoba("Jan", "Kowalski", 1990));
+            osoby.Add(new Osoba("Anna", "Nowak", 1990));
+            osoby.Add(new Osoba("Anna", "Nowacka", 1996));
+            osoby.Add(new Osoba("Tomasz", "Kowalski", 1998));
+            osoby.Add(new Osoba("Jakub", "Wolski", 1992));
+            osoby.Add(new Osoba("Justyna", "Kowalska", 1991));
+            osoby.Add(new Osoba("Alicja", "Krawiec", 1999));
+            osoby.Add(new Osoba("Tomasz", "Nowak", 1998));
+            osoby.Add(new Osoba("Jan", "Kowalski", 1994));
+            osoby.Sort();
+            foreach(var element in osoby)
+            {
+                Console.WriteLine(element.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
